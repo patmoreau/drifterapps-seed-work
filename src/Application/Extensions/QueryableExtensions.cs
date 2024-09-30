80a621fb2ea3273322431240abc557963c700786
Patrick Moreau <patrick@drifterapps.com>
2024-09-30 13:53:40 -0400

    Add primitive casting to Query (#317)

--- src/Application/Extensions/QueryableExtensions.cs
+++ src/Application/Extensions/QueryableExtensions.cs
@@ -1,6 +1,5 @@
 using System.Linq.Dynamic.Core;
-using System.Reflection;
-using DrifterApps.Seeds.Domain;
+using DrifterApps.Seeds.Application.Converters;
 
 namespace DrifterApps.Seeds.Application.Extensions;
 
@@ -32,12 +31,9 @@ public static class QueryableExtensions
                 _ => throw new NotSupportedException($"Invalid comparison operator '{@operator}'.")
             };
 
-            if (IsPrimitiveType<T>(property))
-            {
-                property = $"{property}.Value";
-            }
+            var queryValue = PrimitiveTypeConverter.ConvertFromPrimitiveType<T>(property, value);
 
-            query = query.Where($"{property} {comparison} @0", value);
+            query = query.Where($"{property} {comparison} @0", queryValue);
         }
 
         // Apply sorting
@@ -53,12 +49,6 @@ public static class QueryableExtensions
                         var field = match.Groups["field"].Value;
                         var direction = string.IsNullOrWhiteSpace(match.Groups["desc"].Value) ? "ASC" : "DESC";
 
-
-                        if (IsPrimitiveType<T>(field))
-                        {
-                            field = $"{field}.Value";
-                        }
-
                         return $"{field} {direction}";
                     }
                     default:
@@ -73,26 +63,5 @@ public static class QueryableExtensions
         query = query.Skip(queryParams.Offset).Take(queryParams.Limit);
 
         return query;
-    }
-
-    private static bool IsPrimitiveType<T>(string property)
-    {
-        // Get the property type to check if it's a IPrimitiveType
-        var propertyInfo = typeof(T).GetProperty(property,
-            BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-        if (propertyInfo is null)
-        {
-            return false;
-        }
-
-        var propertyType = propertyInfo.PropertyType;
-        if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(IPrimitiveType<>))
-        {
-            return true;
-        }
-
-        var interfaces = propertyType.GetInterfaces();
-        return Array.Exists(interfaces, @interface =>
-            @interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IPrimitiveType<>));
     }
 }
