d440af24894844fcf018fafdf4140f9f0f1e83e7
Patrick Moreau <patrick@drifterapps.com>
2025-01-17 14:33:15 -0500

    refactor: simplify ApiResponseAssertions and related classes by removing unnecessary parameters

--- src/Testing/FluentAssertions/StronglyTypedIdEquivalencyStep.cs
+++ src/Testing/FluentAssertions/StronglyTypedIdEquivalencyStep.cs
@@ -13,21 +13,21 @@ public class StronglyTypedIdEquivalencyStep : IEquivalencyStep
     /// </summary>
     /// <param name="comparands">The comparands containing the subject and expectation.</param>
     /// <param name="context">The equivalency validation context.</param>
-    /// <param name="valueChildNodes">The nested equivalency validator.</param>
+    /// <param name="nestedValidator">The nested equivalency validator.</param>
     /// <returns>
     ///     An <see cref="EquivalencyResult" /> indicating whether the comparison was handled or should continue.
     /// </returns>
     public EquivalencyResult Handle(Comparands comparands, IEquivalencyValidationContext context,
-        IValidateChildNodeEquivalency valueChildNodes)
+        IEquivalencyValidator nestedValidator)
     {
         switch (comparands)
         {
             case {Subject: Guid subjectGuid, Expectation: IStronglyTypedId expectedStronglyTypedId}:
                 subjectGuid.Should().Be(expectedStronglyTypedId.Value);
-                return EquivalencyResult.EquivalencyProven; // Indicating that the comparison is handled
+                return EquivalencyResult.AssertionCompleted; // Indicating that the comparison is handled
             case {Subject: IStronglyTypedId subjectStronglyTypedId, Expectation: Guid expectedGuid}:
                 subjectStronglyTypedId.Value.Should().Be(expectedGuid);
-                return EquivalencyResult.EquivalencyProven; // Indicating that the comparison is handled
+                return EquivalencyResult.AssertionCompleted; // Indicating that the comparison is handled
             default:
                 return EquivalencyResult.ContinueWithNext; // Indicating that the comparison is not handled
         }
